c4edd4ebac88201a6cb05d38dc106549cdb19cab
Fabian Oudhaarlem <fabian@coqtail.com>
2015-02-18 13:59:07 +0100

    remove a check for themes to be removed since this does not fullfill a purpose, no multiple themes can exists at the same time

--- lib/deface/environment_decorator.rb
+++ lib/deface/environment_decorator.rb
@@ -3,11 +3,9 @@
 Deface::Environment::Overrides.class_eval do
 
   def load_overrides(railtie)
-    unless SpreeMultiDomainThemes.themes_to_remove.collect{ |theme| railtie.root.to_s.include?(theme) }.any?
-      Deface::Override.current_railtie = railtie.class.to_s
-      paths = railtie.respond_to?(:paths) ? railtie.paths["app/overrides"] : nil
-      enumerate_and_load(paths, railtie.root)
-    end
+    Deface::Override.current_railtie = railtie.class.to_s
+    paths = railtie.respond_to?(:paths) ? railtie.paths["app/overrides"] : nil
+    enumerate_and_load(paths, railtie.root)
   end
 
 end
