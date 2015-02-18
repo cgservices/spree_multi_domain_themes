a8b740e4504a99903635ad13070f591b6708c32c
Fabian Oudhaarlem <fabian@coqtail.com>
2015-02-18 11:17:03 +0100

    add the set_view_paths method to the ActionView class instead of the AbstractController since the latter is deprecated

--- lib/actionview/view_paths_decorator.rb
+++ lib/actionview/view_paths_decorator.rb
@@ -1,6 +1,6 @@
 # this decorator sets the new view path array in the lookup_context so the application has the correct view paths
 # everywhere it looks for a specific view
-AbstractController::ViewPaths.module_eval do
+ActionView::ViewPaths.module_eval do
 
   def set_view_paths
     lookup_context.view_paths = SpreeMultiDomainThemes.get_view_paths(lookup_context.view_paths)
