12107c2dfb83eb9c89d72e4e4b066a2b1d3f1e75
Fabian Oudhaarlem <fabian@coqtail.com>
2015-02-23 11:44:10 +0100

    only for view paths use the deface theming and for the other railties use normal deface folder

--- lib/deface/environment_decorator.rb
+++ lib/deface/environment_decorator.rb
@@ -5,7 +5,12 @@ Deface::Environment::Overrides.class_eval do
   def load_overrides(railtie)
     unless SpreeMultiDomainThemes.themes_to_remove.collect { |theme| railtie.root.to_s.include?(theme) }.any?
       Deface::Override.current_railtie = railtie.class.to_s
-      enumerate_and_load(["app/overrides/#{SpreeMultiDomainThemes.current_theme}"], railtie.root)
+      paths = if railtie.paths.path == Rails.root
+                ["app/overrides/#{SpreeMultiDomainThemes.current_theme}"]
+              else
+                railtie.respond_to?(:paths) ? railtie.paths["app/overrides"] : nil
+              end
+      enumerate_and_load(paths, railtie.root)
     end
   end
 
